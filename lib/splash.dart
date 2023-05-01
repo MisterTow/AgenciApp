0f905473b51ee07480d69bb42fa4dc181de1b49d
MisterTow <132258346+MisterTow@users.noreply.github.com>
2023-05-01 09:54:14 -0400

    Reparar el assets y agregar imagen

--- lib/splash.dart
+++ lib/splash.dart
@@ -10,6 +10,21 @@ class splash extends StatefulWidget {
 class _splashState extends State<splash> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFFFF800B),
+              Color(0xFFCE1010),
+            ], //color ojo
+          ),
+        ),
+      ),
+    );
   }
 }
